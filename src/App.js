75eddce9b63454eaa89338de54728b87bff9e769
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-11-26 18:04:09 -0500

    assets and axios base url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React from 'react';
 import './App.css';
+import { weatherApi } from './helpers';
+
+const apiKey = process.env.REACT_APP_API_KEY;
 
 function App() {
+  React.useEffect(() => {
+    const getWeather = async () => {
+      const forecast = await weatherApi.get(
+        `weather?appid=${apiKey}&q=asuncion`
+      );
+      console.log(forecast);
+    };
+
+    getWeather();
+  }, []);
+
   return <div className="App"></div>;
 }
 
