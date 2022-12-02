88e9e81ebfe424f18289b418c3078c19e59f872d
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-12-01 20:51:54 -0500

    feat: more styling added

--- src/components/TodayHighlights/TodayHighlights.js
+++ src/components/TodayHighlights/TodayHighlights.js
@@ -4,12 +4,12 @@ import './TodayHighlights.scss';
 export const TodayHighlights = () => {
   return (
     <section className="weather-app__todays-highlights">
+      <h2>Today's Highlights</h2>
       <div className="weather-app__todays-highlights__container">
-        <h2>Today's Highlights</h2>
         <div className="weather-app__todays-highlights__wind-status">
           <h3>Wind Status</h3>
           <span className="weather-app__todays-highlights__wind-status__speed">
-            7mph
+            7<span>mph</span>
           </span>
           <span className="weather-app__todays-highlights__wind-status__direction">
             WSW
@@ -18,7 +18,7 @@ export const TodayHighlights = () => {
         <div className="weather-app__todays-highlights__humidity">
           <h3>Humidity</h3>
           <span className="weather-app__todays-highlights__humidity__percentage">
-            84%
+            84 <span>%</span>
           </span>
           <div className="weather-app__todays-highlights__humidity__percentage-bar">
             percentage bar
