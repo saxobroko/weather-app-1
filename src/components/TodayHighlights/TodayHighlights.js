c3332c7dc1dff7c06dd3e8e862feaf29d36ffd90
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-12-01 20:36:43 -0500

    feat: today-highlights setup

--- src/components/TodayHighlights/TodayHighlights.js
+++ src/components/TodayHighlights/TodayHighlights.js
@@ -6,10 +6,36 @@ export const TodayHighlights = () => {
     <section className="weather-app__todays-highlights">
       <div className="weather-app__todays-highlights__container">
         <h2>Today's Highlights</h2>
-        <div className="weather-app__todays-highlights__wind-status"></div>
-        <div className="weather-app__todays-highlights__humidity"></div>
-        <div className="weather-app__todays-highlights__visibility"></div>
-        <div className="weather-app__todays-highlights__air-pressure"></div>
+        <div className="weather-app__todays-highlights__wind-status">
+          <h3>Wind Status</h3>
+          <span className="weather-app__todays-highlights__wind-status__speed">
+            7mph
+          </span>
+          <span className="weather-app__todays-highlights__wind-status__direction">
+            WSW
+          </span>
+        </div>
+        <div className="weather-app__todays-highlights__humidity">
+          <h3>Humidity</h3>
+          <span className="weather-app__todays-highlights__humidity__percentage">
+            84%
+          </span>
+          <div className="weather-app__todays-highlights__humidity__percentage-bar">
+            percentage bar
+          </div>
+        </div>
+        <div className="weather-app__todays-highlights__visibility">
+          <h3>Visibility</h3>
+          <span className="weather-app__todays-highlights__visibility__amount">
+            6,4 miles
+          </span>
+        </div>
+        <div className="weather-app__todays-highlights__air-pressure">
+          <h3>Air Pressure</h3>
+          <span className="weather-app__todays-highlights__air-pressure__level">
+            998 mb
+          </span>
+        </div>
       </div>
     </section>
   );
